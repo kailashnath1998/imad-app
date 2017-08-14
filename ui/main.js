7e97ee44b06e73bd23fbee4174c3d3969630272f
Kailashnath N <kailashnath1998@gmail.com>
2017-08-14 13:15:40 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,7 +1,10 @@
 console.log('Loaded!');
 
 var img = document.getElementById('img');
+var button = document.getElementById('counter');
+var span = documnet.getElementById('count');
 var marginLeft = 0;
+var counter = 0;
 
 function moveRight() {
     marginLeft = marginLeft + 10;   
@@ -12,3 +15,8 @@ img.onclick = function() {
     var interval = setInterval(moveRight, 50);
     //img.style.marginLeft = '100px';  
 };
+
+button.onclick = function() {
+      
+};
+
