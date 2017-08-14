1a5563e4beda9b9837d0fe451dd051e0e74251eb
Kailashnath N <kailashnath1998@gmail.com>
2017-08-14 11:25:44 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -9,6 +9,10 @@ app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
 
+app.get('/aone', function (req, res) {
+  res.sendFile('under construction');
+});
+
 app.get('/ui/style.css', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'style.css'));
 });
