843f160656608924aed89e1fea92e0b7c3d35309
Kailashnath N <kailashnath1998@gmail.com>
2017-08-14 12:46:01 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -9,6 +9,6 @@ function moveRight() {
 }
 
 img.onclick = function() {
-    var interval = setInterval(moveRight, 1);
+    var interval = setInterval(moveRight, 50);
     //img.style.marginLeft = '100px';  
 };
