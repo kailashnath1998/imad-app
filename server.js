14ab61f008953746e4f4ddd4034e5a01e5e2c2c5
Kailashnath N <kailashnath1998@gmail.com>
2017-08-14 15:22:45 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -26,10 +26,6 @@ app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
 
-app.get('/aone', function (req, res) {
-  res.sendFile(path.join(__dirname, 'ui', 'aone.html'));
-});
-
 app.get('/resetc', function (req, res) {
   counter=0;
   res.send('sucess');
@@ -46,6 +42,58 @@ app.get('/ui/style.css', function (req, res) {
 app.get('/ui/madi.png', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'madi.png'));
 });
+
+function createTemplet(data){
+    var title = data.title;
+    var date = data.date;
+    var content = data.content;
+    
+    var reply = 
+        `<html>
+            <head>
+                <title>${title} | Kailashnath</title>
+                <meta name="viewport" content="width=device-width initial-scale=1" />
+                <link href="/ui/style.css" rel="stylesheet" />
+            </head>
+            <body>
+                <div class="container">
+                    <div>
+                        <a href="/">Home</a>
+                    </div>
+                    <hr/>
+                    <h3>
+                        ${title}
+                    </h3>
+                    <div>
+                        ${date}
+                    </div>
+                    <div>
+                        <p>
+                            ${content}
+                        </p>
+                        <p>
+                            Content1---Content1---Content1---Content1---Content1---Content1
+                        </p>
+                    </div>
+                </div>
+            </body>
+        </html>`;
+    return reply;
+
+}
+
+var articles = {
+    aone: {
+        title : 'AONE',
+        date : '14 Aug, 2017',
+        content : 'content---content---content---content'
+    }
+};
+app.get('/:aname', function (req, res) {
+    var an = req.parms.aname;
+    res.send(createTemplet(articles[an]));
+});
+
 
 
 // Do not change port, otherwise your app won't run on IMAD servers
