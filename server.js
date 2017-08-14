0d99777a288e6e99a7eba7db043c353eae8f69a3
Kailashnath N <kailashnath1998@gmail.com>
2017-08-14 15:06:43 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -15,6 +15,13 @@ app.get('/cdisp', function (req, res) {
   res.send(counter.toString());
 });
 
+var names = [];
+app.get('/submit-name', function (req, res) {
+  var name = req.query.name;
+  names.push(name);
+  res.send(JSON.stringify(names));
+});
+
 app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
